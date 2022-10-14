4425fe22cbf167470f4467041cd355533f53af0d
Kevin <KXD200014@utdallas.edu>
2022-10-14 16:32:28 -0500

    Changed user schema

--- api/src/schema/user.ts
+++ api/src/schema/user.ts
@@ -4,18 +4,12 @@ import express from "express";
 export interface ICreateUserSchema extends express.Request {
   body: {
     netID: string;
-    numChild: Number;
-    numParent: Number;
-    numElderly: Number;
-    familyIncome: Number;
+    
   };
 }
 export const CreateUserSchema = Joi.object({
   body: Joi.object({
     netID: Joi.string().required().lowercase().trim().min(9).max(9),
-    numChild: Joi.number().required(),
-    numParent: Joi.number().required(),
-    numElderly: Joi.number().required(),
-    familyIncome: Joi.number().required()
+    
   })
 });
