083dee0204e76047dd0edcfad4eef6c862b2ccaf
Kevin <KXD200014@utdallas.edu>
2022-10-26 18:17:54 -0500

    worker log routes

--- api/src/routes/workerLogs.ts
+++ api/src/routes/workerLogs.ts
@@ -1,8 +1,8 @@
 import express from "express";
-import status from "http-status";
+//import status from "http-status";
 import { validate as validateSchema, MongoIdSchema, IMongoIdSchema } from "../schema";
 import * as schema from "../schema/workerLogs";
-import { WorkerLogs } from "../models";
+//import { WorkerLogs } from "../models";
 
 export const router = express.Router();
 
@@ -10,14 +10,8 @@ router.post("/", validateSchema(schema.CreateWorkerLogSchema), async (req: schem
     res.status(501).send({message: "NOT_IMPLEMENTED"});
 });
 
-router.put("/:id", validateSchema(MongoIdSchema), async(req: IMongoIdSchema, res, next) => {
-    res.status(501).send({message: "NOT_IMPLEMENTED"});
-});
 
 router.get("/", async (req, res, next) => {
     res.status(501).send({message: "NOT_IMPLEMENTED"});
 });
 
-router.get("/:id", validateSchema(MongoIdSchema), async(req: IMongoIdSchema, res, next) => {
-    res.status(501).send({message: "NOT_IMPLEMENTED"});
-});
