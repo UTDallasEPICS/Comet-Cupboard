8afb59c051ea7ec83c57e7d1c8009548bf752274
Nikhil Jahagirdar <nxj200009@utdallas.edu>
2022-11-07 20:06:33 -0600

    updated item schema.

--- api/src/schema/item.ts
+++ api/src/schema/item.ts
@@ -3,14 +3,19 @@ import express from "express";
 
 export interface ICreateItemSchema extends express.Request {
   body: {
-    expDate: string;
-    donor: string;
-   
+    expDate: string,
+    item: string, //objectid
+    quantity: string,
+    donor: string, //donors
+    actionType: string, //Tracking the changes
   };
 }
 export const CreateItemSchema = Joi.object({
   body: Joi.object({
     expDate: Joi.string().required(),
+    item: Joi.string().required(),
+    quantity: Joi.string().required(),
     donor: Joi.string().required(),
+    actionType: Joi.string().required()
   })
 });
