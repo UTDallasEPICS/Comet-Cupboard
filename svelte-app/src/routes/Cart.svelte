09081b75a84a276818f156f287f022779154d1c8
Mya Burris <mmb190012@utdallas.edu>
2022-11-08 09:54:01 -0600

    Added header, footer, title, content placeholders

--- svelte-app/src/routes/Cart.svelte
+++ svelte-app/src/routes/Cart.svelte
@@ -1,11 +1,71 @@
-<script>
-    
-</script>
-  
-  <div>
-    <p>test cart</p>
-  </div>
-  
-<style>
+<!-- 
+  This file is for building the Cart Page
+-->
 
+<!--JAVASCRIPT HERE-->
+<script>
+  //components that are imported from Figma designs
+    import TopBar from '../components/TopBar.svelte'
+    import Footer from '../components/Footer.svelte'
+</script>
+
+<!-- HTML HERE-->
+<div class="flex-wrapper">
+  <!--header-->
+  <div class="header">
+    <TopBar />
+  </div>
+  <!-- title of page-->
+  <h1>Cart</h1>
+
+  <div class="content">
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+    <p>content</p>
+  </div>
+
+  <!-- footer -->
+  <div class="footer">
+    <Footer />
+  </div>
+</div>
+
+<!-- CSS HERE-->
+<style>
+  /* makes header stay on top of page at all times */
+  .header {
+    position: sticky;
+    top: 0;
+  }
+
+  /*push footer to the bottom when content overflows the page 
+  (aka you need to scroll down the page) and when content is short*/
+  .footer {
+    margin-top: auto; 
+  }
 </style>
