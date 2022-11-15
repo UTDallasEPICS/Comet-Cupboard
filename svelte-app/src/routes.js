a35592929abea96ec409574c7ffa73ad2e92b973
Sophie Hoang <sophie.t.hoang@gmail.com>
2022-11-14 19:14:58 -0600

    deleted commented out code and fixed page name in routes.js

--- svelte-app/src/routes.js
+++ svelte-app/src/routes.js
@@ -1,10 +1,10 @@
 import Cart from './routes/Cart.svelte';
-import Checkout from './routes/ShopForItems.svelte';
+import ShopForItems from './routes/ShopForItems.svelte';
 import NotFound from './routes/NotFound.svelte';
 
 export default {
     // exact path
-    '/': Checkout,
+    '/': ShopForItems,
     '/cart': Cart,
     // The catch-all route must always be last
     '*': NotFound
