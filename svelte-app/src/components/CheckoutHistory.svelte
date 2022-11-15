4c9922f2bace13d4fc85edb7437e703489c9cc33
Sophie Hoang <sophie.t.hoang@gmail.com>
2022-11-14 19:15:26 -0600

    added CheckoutHistory and ItemCard components

--- svelte-app/src/components/CheckoutHistory.svelte
+++ svelte-app/src/components/CheckoutHistory.svelte
@@ -1,16 +1,62 @@
 <!-- displays user's checkout history -->
-<script>
-
-
-</script>
+<script lang="ts">
+    import Select, { Option } from '@smui/select';
+    import SortIcon from '../../public/images/sort-icon.png';
+    import Icon from '@smui/select/icon';
+    import ItemCard from './ItemCard.svelte';
+  
+    let fruits = ['Apple', 'Orange', 'Banana', 'Mango'];
+  
+    let value = '';
+    let valueHelperText = '';
+    let valueLeadingIcon = '';
+    let valueInvalid = '';
+  </script>
 
 <div>
     <h1>Checkout History</h1>
-    
+    <Select variant="filled" bind:value={valueLeadingIcon} label="Leading Icon">
+        <!-- <img src={SortIcon} class="sort-icon" alt="Sort icon." /> -->
+        <Icon class="material-icons" slot="leadingIcon">event</Icon>
+        <Option value="" />
+        {#each fruits as fruit}
+          <Option value={fruit}>{fruit}</Option>
+        {/each}
+      </Select>
+  
+      <pre class="status">Selected: {valueLeadingIcon}</pre>
+    <div class="item-list">
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+    </div>
 </div>
 
 <style>
-h1 {
-  font-size: 35px;
-}
+  .item-list {
+    /* grid styles */
+    display: grid;
+    column-gap: 1rem;
+    row-gap: 2rem;
+    grid-template-columns: repeat(3, minmax(150px, 1fr));
+    grid-template-rows: repeat(6, 1fr);
+    grid-auto-flow: dense;
+    /* grid-gap: 2rem; */
+    /* place-items: baseline normal; */
+    align-items: start;
+    justify-items: center;
+    /* justify-content: space-between; */
+    margin: 40px;
+  }
+
+  h1 {
+    font-size: 35px;
+  }
+
+  .sort-icon {
+    width: 25px;
+  }
 </style>
---
+++ svelte-app/src/components/ItemCard.svelte
@@ -0,0 +1,40 @@
+<script>
+
+</script>
+
+<div>
+    <div class="white-box">
+            <h1>Image Coming Soon</h1>
+    </div>
+    <p><b>Item name</b></p>
+</div>
+
+<style>
+  .white-box {
+    width: 150px;
+    height: 150px;
+
+    background-color: white;
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
+  }
+
+  .white-box h1 {
+    margin: 0;
+    padding-top: 45px;
+    padding-left: 15px;
+    padding-right: 15px;
+    color: rgb(139, 139, 139);
+    font-size: 20px;
+    
+    /* text-shadow: rgb(61, 61, 61) 1px 0 10px; */
+  }
+
+  p {
+    font-size: 20px;
+    word-wrap: break-word;
+    width: 150px;
+  }
+</style>
