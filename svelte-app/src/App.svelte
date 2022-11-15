12a60cbc520002db0e924945501bddb6f72ca477
Sophie Hoang <sophie.t.hoang@gmail.com>
2022-11-14 19:14:16 -0600

    added smui to use button and select menu

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,3 +1,15 @@
+<svelte:head>
+	<!-- Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700">
+	
+  <!-- Material Typography -->
+	<link rel="stylesheet" href="https://unpkg.com/@material/typography@14.0.0/dist/mdc.typography.css" />
+	
+	<!-- SMUI -->
+	<link rel="stylesheet" href="https://unpkg.com/svelte-material-ui/bare.css" />
+</svelte:head>
+
 <script>
   import Router from 'svelte-spa-router';
 	import routes from './routes';
