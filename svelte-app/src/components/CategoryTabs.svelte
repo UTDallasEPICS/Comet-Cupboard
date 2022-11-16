4a4353b10e0b93abc9b1eaf34d8e785ed7fd790f
Sophie Hoang <sophie.t.hoang@gmail.com>
2022-11-16 15:31:39 -0600

    added styling to category names in category tab

--- svelte-app/src/components/CategoryTabs.svelte
+++ svelte-app/src/components/CategoryTabs.svelte
@@ -4,20 +4,21 @@
 
 
 <div>
-<p>Pantry Staples</p>
-<p>Snacks</p>
-<p>Grains</p>
-<p>Breakfast Grains</p>
-<p>Soup</p>
-<p>Protein</p>
-<p>Household Items</p>
-<p>Personal Care</p>
-<p>Fruits</p>
-<p>Vegetables</p>
+    <p>Pantry Staples</p>
+    <p>Snacks</p>
+    <p>Grains</p>
+    <p>Breakfast Grains</p>
+    <p>Soup</p>
+    <p>Protein</p>
+    <p>Household Items</p>
+    <p>Personal Care</p>
+    <p>Fruits</p>
+    <p>Vegetables</p>
 </div>
 
 <style>
     p {
-        text-align: left;
+        text-align: center;
+        color: white;
     }
 </style>
