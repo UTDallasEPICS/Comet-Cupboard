13211cd9c63c94eec58b1b9ed0473fc44d672452
Hamzah Ismail <hamzah.433@gmail.com>
2022-11-16 15:30:32 -0600

    Finalized Employee routes and updated schema

--- api/src/routes/employee.ts
+++ api/src/routes/employee.ts
@@ -1,23 +1,83 @@
+//Route endpoint for all things related to Employee
 import express from "express";
-//import status from "http-status";
+import status from "http-status";
 import { validate as validateSchema, MongoIdSchema, IMongoIdSchema } from "../schema";
 import * as schema from "../schema/employee";
-//import { Employee } from "../models";
+import { Employee } from "../models";
 
 export const router = express.Router();
 
+//NetID
 router.post("/", validateSchema(schema.CreateEmployeeSchema), async (req: schema.ICreateEmployeeSchema, res, next) => {
-    res.status(501).send({message: "NOT_IMPLEMENTED"});
-});
-
-router.put("/:id", validateSchema(MongoIdSchema), async(req: IMongoIdSchema, res, next) => {
-    res.status(501).send({message: "NOT_IMPLEMENTED"});
-});
-
+    try {
+      const existingUser = await Employee.findOne({ netID: req.body.netID });
+      if (existingUser) {
+        return next({
+          message: "Account already exists",
+          status: status.BAD_REQUEST
+        });
+      }
+      const employee = new Employee({
+        netID: req.body.netID, 
+        password: req.body.password
+        
+      });
+      await employee.save();
+      // remember to send a response back to whoever is requesting or else it will infinitely wait
+      res.send({ message: "Successfully created account!", id: employee._id });
+    } catch (e) {
+      next(e);
+    }
+  });
+  
+    // get all accounts
 router.get("/", async (req, res, next) => {
-    res.status(501).send({message: "NOT_IMPLEMENTED"});
-});
+    try {
+      const employees = await Employee.find();
+      res.send({ accounts: employees });
+    } catch (e) {
+      next(e);
+    }
+  });
+    
+    // get one account
+    router.get("/:id", validateSchema(MongoIdSchema), async (req: IMongoIdSchema, res, next) => {
+        try {
+          if (!req.params.id) {
+            return next({ message: "Id is required", status: status.BAD_REQUEST });
+          }
+          const existingUser = await Employee.findOne({ _id: req.params.id });
+          if (!existingUser) {
+            return next({
+              message: "Account not found",
+              status: status.BAD_REQUEST
+            });
+          }
+      
+          res.send({ account: existingUser });
+        } catch (e) {
+          next(e);
+        }
+    });
 
-router.get("/:id", validateSchema(MongoIdSchema), async(req: IMongoIdSchema, res, next) => {
-    res.status(501).send({message: "NOT_IMPLEMENTED"});
-});
+    // delete new account
+    router.delete("/:id", validateSchema(schema.CreateEmployeeSchema), async (req, res, next) => {
+      try {
+      if (!req.params.id) {
+        return next({ message: "Id is required", status: status.BAD_REQUEST });
+      }
+      await Employee.deleteOne({ _id: req.params.id });
+  
+      res.send({ message: "Successfully deleted account" });
+    } catch (e) {
+      next(e);
+    }
+  });
+
+// router.get("/", async (req, res, next) => {
+//     res.status(501).send({message: "NOT_IMPLEMENTED"});
+// });
+
+// router.get("/:id", validateSchema(MongoIdSchema), async(req: IMongoIdSchema, res, next) => {
+//     res.status(501).send({message: "NOT_IMPLEMENTED"});
+// });
