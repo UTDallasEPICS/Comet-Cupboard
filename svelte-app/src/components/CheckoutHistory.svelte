6d1ac5250e4fde344079c5d1f6f38dec025ba931
Sophie Hoang <sophie.t.hoang@gmail.com>
2022-11-18 21:43:01 -0600

    finished styling Sort menu

--- svelte-app/src/components/CheckoutHistory.svelte
+++ svelte-app/src/components/CheckoutHistory.svelte
@@ -1,31 +1,48 @@
 <!-- displays user's checkout history -->
 <script lang="ts">
     import Select, { Option } from '@smui/select';
-    import SortIcon from '../images/sort-icon.png';
     import Icon from '@smui/select/icon';
     import ItemCard from './ItemCard.svelte';
   
-    let fruits = ['Apple', 'Orange', 'Banana', 'Mango'];
+    let sorts = ['Alphabetical','Frequent'];
   
     let value = '';
     let valueHelperText = '';
-    let valueLeadingIcon = '';
+    let valueLeadingIcon = 'Alphabetical'; // sets default value of sort to "Alphabetical"
     let valueInvalid = '';
   </script>
 
 <div>
     <h1>Checkout History</h1>
-    <Select variant="filled" bind:value={valueLeadingIcon} label="Leading Icon">
-        <!-- <img src={SortIcon} class="sort-icon" alt="Sort icon." /> -->
-        <Icon class="material-icons" slot="leadingIcon">event</Icon>
+    <!-- <Select variant="filled" bind:value={valueLeadingIcon} label="Sort">
+        <!- - <img src={SortIcon} class="sort-icon" alt="Sort icon." /> - ->
+        <Icon class="material-icons" slot="leadingIcon">sort</Icon>
         <Option value="" />
-        {#each fruits as fruit}
-          <Option value={fruit}>{fruit}</Option>
+        {#each sorts as sort}
+          <Option value={sort}>{sort}</Option>
         {/each}
       </Select>
   
-      <pre class="status">Selected: {valueLeadingIcon}</pre>
+      <pre class="status">Selected: {valueLeadingIcon}</pre> -->
+
+    <div>
+      <Select
+        class="shaped-outlined"
+        variant="outlined"
+        bind:value={valueLeadingIcon}
+        label="Sort"
+      >
+        <Icon class="material-icons" slot="leadingIcon">sort</Icon>
+        <Option value="" />
+        {#each sorts as sortOption}
+          <Option value={sortOption}>{sortOption}</Option>
+        {/each}
+      </Select>
+  
+      <!-- <pre class="status">Selected: {valueLeadingIcon}</pre> -->
+    </div>
     <div class="item-list">
+      <ItemCard />
       <ItemCard />
       <ItemCard />
       <ItemCard />
@@ -40,9 +57,9 @@
     /* grid styles */
     display: grid;
     column-gap: 1rem;
-    row-gap: 2rem;
+    row-gap: 1rem;
     grid-template-columns: repeat(3, minmax(150px, 1fr));
-    grid-template-rows: repeat(6, 1fr);
+    /* grid-template-rows: repeat(4, 1fr); */
     grid-auto-flow: dense;
     /* grid-gap: 2rem; */
     /* place-items: baseline normal; */
@@ -56,7 +73,13 @@
     font-size: 35px;
   }
 
-  .sort-icon {
-    width: 25px;
+  /* changes color of selected text option in menu */
+  :global(.mdc-select:not(.mdc-select--disabled) .mdc-select__selected-text) {
+      color: #154734 !important;
+  }
+
+  /* changes color of select menu's label when focused */
+  :global(.mdc-select:not(.mdc-select--disabled).mdc-select--focused .mdc-floating-label) {
+    color: #154734 !important;
   }
 </style>
