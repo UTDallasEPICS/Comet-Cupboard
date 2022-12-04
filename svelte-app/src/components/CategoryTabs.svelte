eba42899f93e37b088b3246a4d5d6f3e1fc05224
Christopher Larsen <chrislarsen2001@gmail.com>
2022-12-03 18:07:27 -0600

    made the cat tabs into buttons

--- svelte-app/src/components/CategoryTabs.svelte
+++ svelte-app/src/components/CategoryTabs.svelte
@@ -6,15 +6,25 @@
 
 <div>
     <p>Pantry Staples</p>
+    <input type="button" name = "pantryStaplesbutton">
     <p>Snacks</p>
+    <input type="button" name = "Snacks">
     <p>Grains</p>
+    <input type="button" name = "Grains">
     <p>Breakfast Grains</p>
+    <input type="button" name = "Breakfast Grains">
     <p>Soup</p>
+    <inut type="button" name = "Soup">
     <p>Protein</p>
+    <input type="button" name = "Protein">
     <p>Household Items</p>
+    <input type="button" name = "Household Items">
     <p>Personal Care</p>
+    <input type="button" name = "Personal Care">
     <p>Fruits</p>
+    <input type="button" name = "Fruits">
     <p>Vegetables</p>
+    <input type="button" name = "Vegetables">
 </div>
 
 <style>
