ce89ed6da07154fa1530e28b165d776563f6621f
myaburris2 <mmb190012@utdallas.edu>
2022-12-07 11:48:00 -0600

    fixed touch target issue on Shop for Items button
    Co-authored-by: Sophie Hoang <sophie-th@users.noreply.github.com>

--- svelte-app/src/routes/Cart.svelte
+++ svelte-app/src/routes/Cart.svelte
@@ -16,73 +16,61 @@
 
 <!-- HTML HERE-->
 <div class="flex-wrapper">
-
   <!--header-->
   <div class="header">
     <TopBar />
   </div>
-
-  <!-- Shop for Items back button -->
-  <div class="shop-for-items-button" style="display:flex; flex-wrap:wrap; align-items:center;">
-    <Wrapper>
-      <a href="#/">
-      <Button on:click={() => clicked++} variant="raised" touch>
-        <Icon class="material-icons">arrow_back_ios</Icon>
-        <Label>Shop for Items</Label>
-      </Button>
-      </a>
-    </Wrapper>
+  <div class="container">
+    <div class="side-by-side">
+      <!-- Shop for Items back button -->
+      <div class="shop-for-items-button" style="display:flex; flex-wrap:wrap; align-items:center;">
+        <Wrapper>
+          <a href="#/">
+            <Button on:click={() => clicked++} variant="raised" touch>
+              <Icon class="material-icons">arrow_back_ios</Icon>
+              <Label>Shop for Items</Label>
+            </Button>
+          </a>
+        </Wrapper>
+      </div>
+      <!-- title of page-->
+      <!-- <div class=""> -->
+      <h1>Cart</h1>
+      <!-- </div> -->
+    </div>
   </div>
-
-  <!-- title of page-->
-  <h1>Cart</h1>
-
-  <div class="content">
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-    <p>content</p>
-  </div>
-
   <!-- footer -->
   <div class="footer">
     <Footer />
   </div>
-
 </div>
 
 <!-- CSS HERE -->
 <style>
+   .flex-wrapper {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    /* width: 100%; */
+  }
 
   /*styles header*/
   .header {
+    /* make header stay on top of page at all times */
     position: sticky;
     top: 0;
-    z-index: 1;
+
+    /* make header stay in front of all items */
+    z-index: 2;
+  }
+
+  .container {
+    z-index: 0;  /* make header stay in front of all items */
+
+    /* using flex box to align items to center */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
 
   /*styles footer*/
@@ -90,16 +78,26 @@
     margin-top: auto; 
   }
 
+  .side-by-side {
+    text-align: center;
+    /* align-items: center; */
+
+    /* positions both elements side-by-side */
+    display: flex;
+    justify-content: space-between;
+  }
+
   /*styles Cart title*/
   h1{
-    position: relative;
-    left: -90px;
+    text-align: center;
+    margin-top: 25px;
   }
 
   /*styles Shop for Items button*/
   .shop-for-items-button {
-    float: left;
     margin: 20px 15px;
+    position: absolute;
+    left: 0;
   }
 
   * :global(.mdc-button--raised:not(:disabled)) {
