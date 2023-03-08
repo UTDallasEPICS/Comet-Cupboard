52ceb10155c537820e6b1c1964ec884845a8f2b6
Erin McDill <erinmcdill@gmail.com>
2023-03-07 19:41:19 -0600

    Added the color coding rectangles.

--- ui/src/components/CategoryTabs.svelte
+++ ui/src/components/CategoryTabs.svelte
@@ -63,6 +63,26 @@
   {#each categories as category, index}
     <!-- svelte-ignore a11y-click-events-have-key-events - next semester -->
       <p id="category-text" on:click={() => handleCategoryClick(categories[index])}>{category.name}</p>
+    <!-- rough color coding list for colored rectangles under category text -->
+      {#if category.name === "Pantry Staples"}
+        <div class="pantry"></div>
+      {:else if category.name === "Snacks"}
+        <div class="snacks"></div>
+      {:else if category.name === "Grains"}
+        <div class="grains"></div>
+      {:else if category.name === "Breakfast Grains"}
+        <div class="breakfast"></div>
+      {:else if category.name === "Soup"}
+        <div class="soup"></div>
+      {:else if category.name === "Protein"}
+        <div class="protein"></div>
+      {:else if category.name === "Household Items"}
+        <div class="house"></div>
+      {:else if category.name === "Personal Care"}
+        <div class="care"></div>
+      {:else if category.name === "Fruits"}
+        <div class="fruit"></div>
+      {/if}
   {/each}
 </div>
  
@@ -73,5 +93,68 @@
       cursor: pointer;
 
       padding: 5px;
+  }
+
+  .pantry {
+      margin: auto;
+      height: 5px;
+      width: 65%;
+      background-color: rgb(36, 100, 180);
+  }
+
+  .snacks {
+      margin: auto;
+      height: 5px;
+      width: 65%;
+      background-color: rgb(114, 204, 223);
+  }
+
+  .grains {
+      margin: auto;
+      height: 5px;
+      width: 65%;
+      background-color: rgb(209, 193, 19);
+  }
+
+  .breakfast {
+      margin: auto;
+      height: 5px;
+      width: 65%;
+      background-color: rgb(172, 177, 99);
+  }
+
+  .soup {
+      margin: auto;
+      height: 5px;
+      width: 65%;
+      background-color: rgb(255, 170, 0);
+  }
+
+  .protein {
+      margin: auto;
+      height: 5px;
+      width: 65%;
+      background-color: rgb(255, 0, 0);
+  }
+
+  .house {
+      margin: auto;
+      height: 5px;
+      width: 65%;
+      background-color: rgb(255, 255, 255);
+  }
+
+  .care {
+      margin: auto;
+      height: 5px;
+      width: 65%;
+      background-color: rgb(52, 196, 83);
+  }
+
+  .fruit {
+      margin: auto;
+      height: 5px;
+      width: 65%;
+      background-color: rgb(205, 118, 156);
   }
 </style>
