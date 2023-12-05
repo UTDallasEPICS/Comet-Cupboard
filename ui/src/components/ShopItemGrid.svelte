d252e8251fad47f3433b55c24177b9a132845fae
Felix Jimenez <felixjimenez4444@gmail.com>
2023-12-04 19:19:30 -0600

    replaced shop grid with SMUI layoutgrid

--- ui/src/components/ShopItemGrid.svelte
+++ ui/src/components/ShopItemGrid.svelte
@@ -6,38 +6,52 @@
     
     import ItemCard from './ItemCard.svelte';
     import { createEventDispatcher } from "svelte";
-    import LayoutGrid from '@smui/layout-grid'
-    import {Cell} from '@smui/layout-grid/';
+    import LayoutGrid, {Cell} from "@smui/layout-grid"
     const dispatch = createEventDispatcher();
 
     export let inv = $inventory;
 
-    const handleItemCardClick = (item) => {
-        dispatch("itemCardClicked",item);
+    const handleItemCardClick = (clickItem) => {
+        dispatch("itemCardClicked",clickItem);
     }
 
 </script>
 
 <!-- Maybe a dedicated grid components and the props would be how many columns you want there to be or it asks for a label thingy    -->
-<LayoutGrid class="grid">
-    {#each inv as item, index}
-        {#if $categoryClicked.name == "All" || item.category == $categoryClicked.name}
-        <Cell span={4}>
-            <ItemCard on:click={() => (handleItemCardClick(item))} bind:item={item} />
-        </Cell>
-        {/if}
-    {/each}
-</LayoutGrid>
+<div>
+    <LayoutGrid class="lGrid">
+        {#each inv as item, index}
+            {#if $categoryClicked.name == "All" || item.category == $categoryClicked.name}
+            <Cell span={4} class = "itemCell">
+                <ItemCard on:click={(e) => (handleItemCardClick(item))} {item} />
+            </Cell>
+            {/if}
+        {/each}
+    </LayoutGrid>
+</div>
 <!-- <div id="shop-grid"  style="grid-template-columns: repeat(auto-fit, 1fr);">
             <ItemCard on:click={() => (handleItemCardClick(item))} bind:item={item} />
 </div> -->
 
 <style>
 
-* :global(.grid) {
-    margin: 0;
+div {
+    width: 100%;
+    max-width: 1200px;
+    margin: 0 auto;
 }
 
+* :global(.lGrid) {
+    margin: 0;
+    width:100%;
+}
+
+* :global(.itemCell) {
+    padding: 0;
+    margin: 0;
+    width:100%;
+    height:100%;
+}
 /* #shop-grid {
     background-color: aqua;
     box-sizing: border-box;
