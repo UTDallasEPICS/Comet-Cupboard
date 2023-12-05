7c7f0ec261e2219f2f2dec07832c68e0be906b9e
Felix Jimenez <felixjimenez4444@gmail.com>
2023-12-04 20:25:24 -0600

    changed directory style

--- ui/src/routes/NTFBform.svelte
+++ ui/src/routes/NTFBform.svelte
@@ -7,8 +7,7 @@
     import CheckoutHistory from '../components/CheckoutHistory.svelte';
     import CategoryTabs from '../components/CategoryTabs.svelte';
     import CategorySection from '../components/CategorySection.svelte';
-    import NorthTexasFoodBankForm from 'src/components/NorthTexasFoodBankForm.svelte';
-  
+    import NorthTexasFoodBankForm from '../components/NorthTexasFoodBankForm.svelte';
   </script>
 
 <div class="flex-wrapper">
