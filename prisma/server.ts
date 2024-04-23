fc207b86d7d29ca9226cf0289262b5364789e48c
crlinpost <lincrystalhome@gmail.com>
2024-04-23 13:01:24 -0500

    continue

--- prisma/server.ts
+++ prisma/server.ts
@@ -8,16 +8,18 @@ const app: Express = express();
 
 
 async function main() {
-    const students = await prisma.student.findMany();
-    console.log(students)
+    // const students = await prisma.student.findMany();
+    // console.log(students) //Prints all the students in student table
   // ... you will write your Prisma Client queries here
   //npx ts-node prisma/server.ts unless in this directory
 }
 
 main()
   .then(async () => {
+
     await prisma.$disconnect()
   })
+  
   .catch(async (e) => {
     console.error(e)
     await prisma.$disconnect()
