c250a7d448b37e3a98d24711f213ba57607db8b7
crlinpost <lincrystalhome@gmail.com>
2024-04-23 15:48:28 -0500

    Starting building item API, added dist gitignore.

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,10 @@ import {status} from '@prisma/client'
 
 
 const prisma = new PrismaClient()
-// const app: Express = express();
+const port: number = 8000;
+const app: Express = express();
+
+
 
 
 async function main() {
@@ -28,3 +31,7 @@ main()
     await prisma.$disconnect()
     process.exit(1)
   })
+
+  app.listen(port, () => { 
+    console.log("Server started and listening on port ${port}")
+  });
