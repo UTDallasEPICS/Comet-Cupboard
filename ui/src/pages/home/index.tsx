16d7b7d5fd8b8fce123eee48068df2cbdc4bef52
Aryan Kumar <aryanneerajk@gmail.com>
2024-04-24 16:25:13 -0500

    Updated added components to home page

--- ui/src/pages/home/index.tsx
+++ ui/src/pages/home/index.tsx
@@ -1,8 +1,28 @@
 import React from "react";
 import Layout from "../../layout";
-
+import BottomBar from "../../components/BottomBar"
+import image from "../../assets/CometCupboard_transparent.png"
+import styled from "styled-components"
+import Product from "../../components/Product"
+import Testimg from "../../assets/CometCupboard_transparent_orange.png"
+const Wrapper = styled.div`
+  display: flex;
+`
+const ProductWrapper = styled.div`
+  margin-left: 5rem;
+  margin-top: 3rem;
+  display: flex;
+`
 const Home = () => {
-  return <Layout>Home</Layout>;
+  return (
+  <Wrapper>
+    <BottomBar image={image} />
+    <Layout>Home</Layout>
+    <ProductWrapper>
+    <Product image={Testimg} name="item1"></Product>
+    </ProductWrapper>
+  </Wrapper>
+  );
 };
 
 export default Home;
