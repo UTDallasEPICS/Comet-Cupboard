64485447ecf35d8a0420ee6b65326f672b4407f5
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2024-06-24 20:03:34 -0500

    Header and Footer (#31)
    * footer
    
    * header navbar

---
+++ components/CometHeader.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+div.bg-gray-600.flex.flex-row(style="background-color: #154734")
+    TopLeftNavigation
+    img(style="height: 60px" src="/TemporaryCometCupboardTransparent.png")
+    p.text-white(style="margin-left: auto") Account
+    p.text-white Cart
+</template>
---
+++ pages/catalog.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+div.flex.flex-row.items-center.justify-center
+    p catalog
+</template>
+
+<script lang="ts" setup>
+</script>
