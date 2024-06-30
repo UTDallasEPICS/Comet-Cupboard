1b76f1c33ee278921f688bf22a315d75dafbd8f2
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2024-06-30 12:02:48 -0500

    line clamp on item cards

--- components/CartItemCard.vue
+++ components/CartItemCard.vue
@@ -3,11 +3,11 @@ div.border-black.border-8.flex.flex-row.flex-nowrap
     img.aspect-square(class="w-16 lg:w-20" src="/TemporaryCometCupboardTransparent.png" alt="Food")
     div.w-full
         div.flex.flex-row.flex-nowrap
-            p.text-base.overflow-hidden.overflow-ellipsis.break-all(style="height: 3rem") Watermelon Watermelon Watermelon Watermelon
+            p.text-base.break-all.line-clamp-2(style="height: 3rem") Watermeloodqwpoqwkpn Watermelon Watermelon Watermelon
             XCircleIcon.w-8.min-w-8.aspect-square(style="margin-left: auto; margin-bottom: auto")
         div.flex.flex-row.flex-nowrap.justify-end
             MinusCircleIcon.w-8
-            div.w-10
+            p 12
             PlusCircleIcon.w-8
             
 </template>
--- components/ItemCard.vue
+++ components/ItemCard.vue
@@ -3,5 +3,5 @@ div.border-black.border-8
     img.w-full.aspect-square(src="/TemporaryCometCupboardTransparent.png" alt="Food")
     ActionButton.rounded-none.w-full
         p Add to Cart
-    p.text-base.overflow-hidden.overflow-ellipsis.break-all(style="height: 3rem") Watermelon Watermelon Watermelon Watermelon
+    p.text-base.break-all.line-clamp-2(style="height: 3rem") Canned Peaches Watermelon Watermelon Watermelon
 </template>
