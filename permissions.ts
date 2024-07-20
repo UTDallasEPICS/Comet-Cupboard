c7573510fdf6de717f79d1bc2cdf3124bdbe4e1d
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2024-07-19 23:38:42 -0500

    export page/api permissions in root to use in vue and server

---
+++ permissions.ts
@@ -0,0 +1,108 @@
+import fs from "fs"
+import path from "path"
+
+const apiFolder = "server/api"
+const pageFolder = "pages"
+
+enum AccessLevel {
+	PUBLIC = 0,
+	STUDENT = 1,
+	VOLUNTEER = 2,
+	ADMIN = 3,
+}
+
+const pageAccessMap: { [route: string]: AccessLevel } = {
+    // PUBLIC PAGES
+    "/": AccessLevel.PUBLIC,
+    "/donation": AccessLevel.PUBLIC,
+    // STUDENT PAGES
+    "/catalog": AccessLevel.STUDENT,
+    "/cart": AccessLevel.STUDENT,
+    "/questionaire": AccessLevel.STUDENT,
+    // VOLUNTEER PAGES
+    "/inventory/add-item": AccessLevel.VOLUNTEER,
+    "/inventory/delete-item-confirmation": AccessLevel.VOLUNTEER,
+    "/inventory/edit-item-deal": AccessLevel.VOLUNTEER,
+    "/inventory/edit-item": AccessLevel.VOLUNTEER,
+    "/inventory/inventory": AccessLevel.VOLUNTEER,
+    "/verify-cart": AccessLevel.VOLUNTEER,
+    // ADMIN PAGES
+    "/data-analytics": AccessLevel.ADMIN
+}
+const apiAccessMap: { [route: string]: { [method: string]: AccessLevel } } = {
+    // PUBLIC APIS
+    "/api/login": {
+        "POST": AccessLevel.PUBLIC
+    },
+    // // STUDENT APIS
+    "/api/inventory/items": {
+        "GET": AccessLevel.STUDENT
+    },
+    "/api/cart/cart": {
+        "GET": AccessLevel.STUDENT,
+        "POST": AccessLevel.STUDENT
+    },
+    "/api/cart/cartItem": {
+        "POST": AccessLevel.STUDENT,
+        "DELETE": AccessLevel.STUDENT
+    },
+    // // VOLUNTEER APIS
+    "/api/inventory/item": {
+        "POST": AccessLevel.VOLUNTEER,
+        "PUT": AccessLevel.VOLUNTEER,
+        "DELETE": AccessLevel.VOLUNTEER
+    },
+    "/api/inventory/[itemID]": {
+        "GET": AccessLevel.VOLUNTEER
+    }
+    // ADMIN APIS
+}
+
+// ********************************************************************************
+// this is just to check if all permissions for pages and apis are covered
+const getAllLeafFilePaths = (folder: string, existingPaths: Array<string>) => {
+	fs.readdirSync(folder).forEach((file) => {
+		const filePath = path.join(folder, file).replaceAll("\\", "/")
+		const stat = fs.statSync(filePath)
+		if (stat.isFile()) {
+			existingPaths.push(filePath.replaceAll("\\", "/"))
+		} else if (stat.isDirectory()) {
+			getAllLeafFilePaths(filePath, existingPaths)
+		}
+	})
+	return existingPaths
+}
+
+// at this point, it looks like 'pages/cart.vue', want /cart
+const pages = getAllLeafFilePaths(pageFolder, []).map((route) => {
+	const slashIndex = route.indexOf("/")
+	const extensionIndex = route.lastIndexOf(".")
+	return route.substring(slashIndex, extensionIndex)
+})
+// at this point, it looks like 'server/api/cart/cart.get.ts', want /api/cart/cart as well as the HTTP method
+const apis = getAllLeafFilePaths(apiFolder, []).map((route) => {
+	const slashIndex = route.indexOf("/")
+	const extensionIndex = route.lastIndexOf(".")
+	const methodIndex = route.lastIndexOf(".", extensionIndex - 1)
+	return { route: route.substring(slashIndex, methodIndex), method: route.substring(methodIndex + 1, extensionIndex).toUpperCase() }
+})
+
+// TODO: probably should check for index page and index api
+
+const missedPages = pages.filter((route) => {
+	return pageAccessMap[route] == undefined
+})
+const missedApis = apis.filter((route) => {
+	return apiAccessMap[route.route] == undefined || apiAccessMap[route.route][route.method] == undefined
+})
+if (missedPages.length != 0) {
+    console.log("Missing page authorization levels")
+    missedPages.forEach(page => console.log(`${page}`))
+}
+if (missedApis.length != 0) {
+    console.log("Missing api authorization levels")
+    missedApis.forEach(api => console.log(`${api.method} ${api.route}`))
+}
+// ********************************************************************************
+
+export { AccessLevel, pageAccessMap, apiAccessMap }
--- server/middleware/03.authorization.ts
+++ server/middleware/03.authorization.ts
@@ -1,47 +1,4 @@
-enum AccessLevel {
-	PUBLIC = 0,
-	STUDENT = 1,
-	VOLUNTEER = 2,
-	ADMIN = 3,
-}
-
-const pageAccessMap = new Map<string, AccessLevel>()
-const apiAccessMap = new Map<string, Map<string, AccessLevel>>() // Map to method, then to path
-apiAccessMap.set("GET", new Map<string, AccessLevel>())
-apiAccessMap.set("POST", new Map<string, AccessLevel>())
-apiAccessMap.set("PUT", new Map<string, AccessLevel>())
-apiAccessMap.set("DELETE", new Map<string, AccessLevel>())
-
-// PUBLIC PAGES AND APIS
-pageAccessMap.set("/", AccessLevel.PUBLIC)
-pageAccessMap.set("/donation", AccessLevel.PUBLIC)
-apiAccessMap.get("POST")?.set("/api/login", AccessLevel.PUBLIC)
-
-// STUDENT PAGES AND APIS
-pageAccessMap.set("/catalog", AccessLevel.STUDENT)
-pageAccessMap.set("/cart", AccessLevel.STUDENT)
-pageAccessMap.set("/questionaire", AccessLevel.STUDENT)
-apiAccessMap.get("GET")?.set("/api/inventory/items", AccessLevel.STUDENT)
-apiAccessMap.get("GET")?.set("/api/cart/cart", AccessLevel.STUDENT)
-apiAccessMap.get("POST")?.set("/api/cart/cart", AccessLevel.STUDENT)
-apiAccessMap.get("DELETE")?.set("/api/cart/cartItem", AccessLevel.STUDENT)
-apiAccessMap.get("POST")?.set("/api/cart/cartItem", AccessLevel.STUDENT)
-
-// VOLUNTEER PAGES AND APIS
-pageAccessMap.set("/inventory/add-item", AccessLevel.VOLUNTEER)
-pageAccessMap.set("/inventory/delete-item-confirmation", AccessLevel.VOLUNTEER)
-pageAccessMap.set("/inventory/edit-item-deal", AccessLevel.VOLUNTEER)
-pageAccessMap.set("/inventory/edit-item", AccessLevel.VOLUNTEER)
-pageAccessMap.set("/inventory/inventory", AccessLevel.VOLUNTEER)
-pageAccessMap.set("/verify-cart", AccessLevel.VOLUNTEER)
-apiAccessMap.get("DELETE")?.set("/api/inventory/item", AccessLevel.VOLUNTEER)
-apiAccessMap.get("POST")?.set("/api/inventory/item", AccessLevel.VOLUNTEER)
-apiAccessMap.get("PUT")?.set("/api/inventory/item", AccessLevel.VOLUNTEER)
-apiAccessMap.get("GET")?.set("/api/inventory/[itemID]", AccessLevel.VOLUNTEER)
-
-// ADMIN PAGES AND APIS
-pageAccessMap.set("/data-analytics", AccessLevel.ADMIN)
-// apiAccessMap.set({}, AccessLevel.ADMIN)
+import { AccessLevel, pageAccessMap, apiAccessMap } from "~/permissions"
 
 const canAccess = (event, accessLevel: AccessLevel): boolean => {
 	switch (accessLevel) {
@@ -59,18 +16,16 @@ const canAccess = (event, accessLevel: AccessLevel): boolean => {
 }
 
 export default defineEventHandler((event) => {
-	console.log("AUTHORIZING" + ` ${event.method} ${event.path}`)
-	if (pageAccessMap.get(event.path)) {
-		console.log("checking page access")
-		const requiredAccessLevel: AccessLevel = pageAccessMap.get(event.path)
+	if (pageAccessMap[event.path]) {
+		const requiredAccessLevel: AccessLevel = pageAccessMap[event.path]
 		if (!canAccess(event, requiredAccessLevel)) {
-			console.log("cannot access")
 			throw createError({ statusCode: 403, statusMessage: "Unauthorized" })
 		}
-	} else if (apiAccessMap.get(event.method)?.get(event.path)) {
-		const requiredAccessLevel: AccessLevel = apiAccessMap.get(event.method).get(event.path)
+	} else if (apiAccessMap[event.path] && apiAccessMap[event.path][event.method]) {
+		const requiredAccessLevel: AccessLevel = apiAccessMap[event.path][event.method]
 		if (!canAccess(event, requiredAccessLevel)) {
 			throw createError({ statusCode: 403, statusMessage: "Unauthorized" })
 		}
 	}
+	console.log("AUTHORIZING" + ` ${event.method} ${event.path}`)
 })
