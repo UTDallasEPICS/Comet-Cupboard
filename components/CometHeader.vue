a1b0a9642626fecb53e5d619996e2896d268f2a9
Emory Grubbs <104403182+emoryjgrubbs@users.noreply.github.com>
2024-10-04 17:03:31 -0500

    basic nav completed

--- components/CometHeader.vue
+++ components/CometHeader.vue
@@ -1,26 +1,28 @@
 <template lang="pug">
-div.min-h-20.bg-utd-green
-    div.mx-8.flex.flex-row
+div.min-h-20.bg-utd-green.content-center
+    div.mx-4.sm_mx-8.flex.flex-row
         div.sm_hidden.place-self-center.pr-8
-            | ham
-        div.flex.flex-row.space-x-14.py-2.items-center.w-full
+            Bars3Icon.size-10.fill-white.cursor-pointer
+        div.flex.flex-row.h-14.space-x-5.sm_space-x-14.items-center.w-full
             img.h-14(src="/CometCupboardLogo1.png")
-            div.max-sm_hidden.flex.flex-row.space-x-10.overflow-x-scroll.items-center.left-left.text-3xl.font-bold.text-white
-                div.flex.text-nowrap
-                    NuxtLink(to="/shopping").cursor-pointer
+            div.flex-grow.flex-row.space-x-10.overflow-x-scroll.items-center.left-left.text-3xl.font-bold.text-white.text-nowrap
+                div.inline-flex.max-sm_hidden
+                    NuxtLink(to="/shopping").cursor-pointer.text-nowrap
                         | Shopping
-                div.flex.text-nowrap
-                    NuxtLink(to="/verify-carts").cursor-pointer
+                div.inline-flex.max-sm_hidden
+                    NuxtLink(to="/verify-carts").cursor-pointer.text-nowrap
                         | Verify Carts
-                div.flex.text-nowrap
-                    NuxtLink(to="/inventory-management").cursor-pointer
+                div.inline-flex.max-sm_hidden
+                    NuxtLink(to="/inventory-management").cursor-pointer.text-nowrap
                         | Inventory Management
-                div.flex.text-nowrap
-                    NuxtLink(to="/data").cursor-pointer
+                div.inline-flex.max-sm_hidden
+                    NuxtLink(to="/data").cursor-pointer.text-nowrap
                         | Data
-            div.flex-grow.text-right
-                |acc
+            ShoppingBagIcon.size-10.min-w-10.fill-white.cursor-pointer.justify-self-right
+            UserIcon.size-10.min-w-10.fill-white.cursor-pointer.justify-self-right
 </template>
 
 <script lang="ts" setup>
+import { Bars3Icon, ShoppingBagIcon, UserIcon } from '@heroicons/vue/24/solid'
+
 </script>
