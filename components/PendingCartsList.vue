b5d671fa4c64909e639d22b4404d649b3fa9c1b3
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2024-10-05 18:15:51 -0500

    cart selected logic for pending cart list

--- components/PendingCartsList.vue
+++ components/PendingCartsList.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 div.flex.flex-col.gap-y-4.w-72
-	button.h-12.rounded-xl.drop-shadow.cursor-pointer(class="bg-gray-300 hover:bg-orange-400 hover:text-white" v-for="s in tempArr" @click="$emit('update:select-cart', pendingCartID)") 
+	button.h-12.rounded-xl.drop-shadow.cursor-pointer(:class="(s === selectedCart ? ('bg-orange-400 text-white') : 'bg-gray-300')" class="hover:bg-orange-400 hover:text-white" v-for="s in tempArr" @click="selectCart(s)") 
 		div.flex.row.justify-between
 			span.px-4.text-xl.text-left {{s}}
 			span.px-4.text-xl.text-right QTY: 2
@@ -8,37 +8,44 @@ div.flex.flex-col.gap-y-4.w-72
 
 <script lang="ts" setup>
 const tempArr = ref<Array<string>>(["abc123456", "def123456", "ghi123456", "jkl123456", "mno123456", "pqr123456", "stu123456", "vwx123456"])
+const emit = defineEmits(["update:select-cart"])
 
-// const pendingCartIDs = ref<Array<string>>([])
-// const pendingCartUpdates = ref<EventSource>()
+const pendingCartIDs = ref<Array<string>>([])
+const selectedCart = ref<string>("jkl123456")
+const pendingCartUpdates = ref<EventSource>()
 
-// onMounted(async () => {
-// 	await getPendingCarts()
-// })
+onMounted(async () => {
+	await getPendingCarts()
+})
 
-// const getPendingCarts = async () => {
-// 	const pendingCarts = await $fetch("/api/verification/pendingCarts", { method: "GET" })
-// 	pendingCartIDs.value = pendingCarts.map((pendingCart) => {
-// 		return pendingCart.cartID
-// 	})
-// }
+const getPendingCarts = async () => {
+	const pendingCarts = await $fetch("/api/verification/pendingCarts", { method: "GET" })
+	pendingCartIDs.value = pendingCarts.map((pendingCart) => {
+		return pendingCart.cartID
+	})
+}
 
-// if (import.meta.client) {
-// 	// change this to use env later
-// 	// also probably use zod to type check the message...
-// 	pendingCartUpdates.value = new EventSource("http://localhost:3000/api/verification/pendingCartsUpdate")
-// 	pendingCartUpdates.value.onmessage = (event) => {
-// 		const { type, payload } = JSON.parse(event.data)
-// 		if (type === "NEW CART") {
-// 			const cartIDToAdd = payload.cartID
-// 			pendingCartIDs.value.push(cartIDToAdd)
-// 		}
-// 		else if (type === "ACCEPT CART" || type === "REJECT CART") {
-// 			const cartIDToRemove = payload.cartID
-// 			pendingCartIDs.value = pendingCartIDs.value.filter((cartID) => {
-// 				return cartID != cartIDToRemove
-// 			})
-// 		}
-// 	}
-// }
+if (import.meta.client) {
+	// change this to use env later
+	// also probably use zod to type check the message...
+	pendingCartUpdates.value = new EventSource("http://localhost:3000/api/verification/pendingCartsUpdate")
+	pendingCartUpdates.value.onmessage = (event) => {
+		const { type, payload } = JSON.parse(event.data)
+		if (type === "NEW CART") {
+			const cartIDToAdd = payload.cartID
+			pendingCartIDs.value.push(cartIDToAdd)
+		}
+		else if (type === "ACCEPT CART" || type === "REJECT CART") {
+			const cartIDToRemove = payload.cartID
+			pendingCartIDs.value = pendingCartIDs.value.filter((cartID) => {
+				return cartID != cartIDToRemove
+			})
+		}
+	}
+}
+
+const selectCart = (cartID: string) => {
+	selectedCart.value = cartID
+	emit("update:select-cart", cartID)
+}
 </script>
