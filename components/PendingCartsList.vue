b462d04faa24ec05b9aea502fef88604dfe31e5a
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2024-10-06 09:34:51 -0500

    cart preview selection and fix sizing

--- components/PendingCartsList.vue
+++ components/PendingCartsList.vue
@@ -1,18 +1,24 @@
 <template lang="pug">
-div.flex.flex-col.gap-y-4.w-72
+div.flex.flex-col.gap-y-4.min-w-60
 	button.h-12.rounded-xl.drop-shadow.cursor-pointer.hover_bg-utd-orange.hover_text-white(
-		:class="(s === selectedCart ? ('bg-utd-orange text-white') : 'bg-cupboard-lg')" v-for="s in tempArr" @click="selectCart(s)") 
+		:class="((s === selectedCart) ? ('bg-utd-orange text-white') : 'bg-cupboard-lg')" @click="emit('update:select-cart', s)" v-for="s in tempArr") 
 		div.flex.flex-row.justify-between
 			span.px-4.text-xl.text-left {{s}}
 			span.px-4.text-xl.text-right QTY: 2
 </template>
 
 <script lang="ts" setup>
+const props = defineProps({
+	selectedCart: {
+		type: String,
+		required: true,
+	},
+})
+
 const tempArr = ref<Array<string>>(["abc123456", "def123456", "ghi123456", "jkl123456", "mno123456", "pqr123456", "stu123456", "vwx123456"])
 const emit = defineEmits(["update:select-cart"])
 
 const pendingCartIDs = ref<Array<string>>([])
-const selectedCart = ref<string>("")
 const pendingCartUpdates = ref<EventSource>()
 
 onMounted(async () => {
@@ -43,10 +49,5 @@ if (import.meta.client) {
 			})
 		}
 	}
-}
-
-const selectCart = (cartID: string) => {
-	selectedCart.value = cartID
-	emit("update:select-cart", cartID)
 }
 </script>
--- pages/verify-cart.vue
+++ pages/verify-cart.vue
@@ -1,16 +1,26 @@
 <template lang="pug">
-div.flex.flex-row.my-8
-    div.mr-24
+div.flex.flex-row.my-8.flex-wrap.md_flex-nowrap.justify-center.md_justify-normal
+    div.md_mr-6.lg_mr-12(:class="((currentCartIDPreview === 'No cart chosen') ? 'visible' : 'invisible hidden') + ' md_visible md_block'")
         p search
-        PendingCartsList(@update:select-cart="setCartIDPreview")
-    CartVerificationPreview(:cartID="currentCartIDPreview" @update:verified-cart="resetCartIDPreview")
+        PendingCartsList(:selectedCart="currentCartIDPreview" @update:select-cart="setCartIDPreview")
+    XMarkIcon.ml-auto.size-10.stroke-black(:class="((currentCartIDPreview === 'No cart chosen') ? 'invisible hidden': 'visible') + ' md_invisible md_hidden'" @click="resetCartIDPreview")
+    CartVerificationPreview(:class="((currentCartIDPreview === 'No cart chosen') ? 'invisible hidden': 'visible') + ' md_visible md_flex'" 
+        :cartID="currentCartIDPreview" @update:verified-cart="resetCartIDPreview")
 </template>
 
 <script lang="ts" setup>
+import { XMarkIcon } from '@heroicons/vue/24/solid'
+
 const currentCartIDPreview = ref<string>("No cart chosen")
 
 const setCartIDPreview = (cartID: string) => {
-	currentCartIDPreview.value = cartID
+    // deselect cart if already chosen
+    if(currentCartIDPreview.value === cartID) {
+        currentCartIDPreview.value = "No cart chosen"
+    }
+    else {
+        currentCartIDPreview.value = cartID
+    }
 }
 
 const resetCartIDPreview = () => {
