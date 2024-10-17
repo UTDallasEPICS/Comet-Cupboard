47c4b09b13b1af56130696e3de695243b11869f3
Xinlong Hu <118075581+XinlongH6@users.noreply.github.com>
2024-10-17 17:20:11 -0500

    Update questionaire.vue
    SoU & NC + Buttons done

--- pages/questionaire.vue
+++ pages/questionaire.vue
@@ -1,6 +1,20 @@
 <template lang="pug">
-div.flex.flex-row.items-center.justify-center
-    p questions to be implemented
-NavLinkButton.block.w-72.h-min(class="sm:flex-grow-0" style="margin-left: auto; margin-right: auto;" to="/request-verify-cart")
-    p Continue
+div.bbox(style="width: 75%;").box-border.m-auto.max-w-md
+    div.banner(style="background-color: #f1f1f1;").p-4.rounded-lg.text-center.mt-4
+        p.font-bold Thank you for taking part
+        p Please complete this survey to help us improve our program 
+
+div.qbox(style="width: 75%;").box-border.m-auto.max-w-xl
+    div.questions.py-4.items-center 
+        p.font-bold Statement of Understanding: 
+        p I assume any and all risks associated with consuming the items I have selected from the Comet Cupboard. I agree to release UT Dallas from liability if I sustain any health or medical issues as a result of consuming foods taken from the Comet Cupboard. I understand that the Comet Cupboard distributes products that may contain nuts or have been processed in plants that use peanuts and/or tree nuts. I also understand that some of Comet Cupboard items may conflict with my allergies or dietary restrictions. 
+        br
+        p.font-bold Nondiscrimination clause: 
+        p UTD Comet Cupboard does not and shall not discriminate on the basis of race, color, religion (creed), gender, gender expression, age, national origin (ancestry), disability, marital status, sexual orientation, or military status, in any of its activities or operations
+div.bbox(style="width: 75%;").box-border.m-auto
+    div.buttons.flex.justify-center.items-center
+        NuxtLink(to="/cart")
+            button().button.bg-red-negative.text-white.w-40.mx-8.mb-8 Decline        
+        NuxtLink(to="/request-verify-cart")
+            button().button.bg-utd-green.text-white.w-40.mx-8.mb-8 Accept
 </template>
