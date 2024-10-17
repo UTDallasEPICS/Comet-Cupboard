5ca3a7ce006fb977e4ef2529632a5ad4cacb4bb1
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2024-10-17 15:33:36 -0500

    deleted old components and pages (#62)

--- components/NavLinkButton.vue
+++
@@ -1,8 +0,0 @@
-<template lang="pug">
-NuxtLink.rounded-full.text-center.text-white.font-bold(style="background-color: #154734; padding: 10px 20px" :to="to")
-    slot
-</template>
-
-<script setup lang="ts">
-const props = defineProps<{ to: string }>()
-</script>
