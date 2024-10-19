8600b95eb2eadba084c4e978d857a0750e7e3417
Xinlong Hu <118075581+XinlongH6@users.noreply.github.com>
2024-10-19 18:34:40 -0500

    Template charts made
    Started some template charts (bar and pie chart)

---
+++ components/PlotlyChart.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  div(ref="plotElement")
+</template>
+   
+<script>
+   import Plotly from 'plotly.js-dist'
+   
+   export default {
+    name: 'PlotlyChart',
+    props: {
+      data: Array,
+      layout: Object
+    },
+    mounted() {
+      this.renderChart();
+    },
+    methods: {
+      renderChart() {
+        Plotly.newPlot(this.$refs.plotElement, this.data, this.layout);
+      }
+    },
+    watch: {
+      data: 'renderChart',
+      layout: 'renderChart'
+    }
+   }
+</script>
--- pages/data.vue
+++ pages/data.vue
@@ -1,4 +1,51 @@
 <template lang="pug">
-div.flex.flex-row.items-center.justify-center
-    p data
+    #app
+    .chart-container(style="display: flex; justify-content: space-around; flex-wrap: wrap;")        
+        //1st chart
+        PlotlyChart(:data="chartData" :layout="chartLayout")
+
+        //2nd chart
+        PlotlyChart(:data="chartData2" :layout="chartLayout2")
 </template>
+  
+   
+<script>
+   import PlotlyChart from './components/PlotlyChart.vue'
+   
+   export default {
+    name: 'App',
+    components: {
+      PlotlyChart
+    },
+    data() {
+    return {
+      // Data for the first chart
+      chartData: [{
+        x: [1, 2, 3, 4, 5],
+        y: [10, 15, 13, 17, 22],
+        type: 'bar'
+      }],
+      chartLayout: {
+        title: 'Outflux Bar Chart',
+        xaxis: {
+          title: 'Time'
+        },
+        yaxis: {
+          title: 'Items'
+        }
+      },
+      
+      // Data for the second chart
+      chartData2: [{
+        values: [20, 10, 15, 25, 30], 
+        labels: ['A', 'E', 'I', 'O', 'U'],
+        hole: .4,
+        type: 'pie'
+      }],
+      chartLayout2: {
+        title: 'Pie Chart'
+      }
+    }
+  }
+}
+</script>
