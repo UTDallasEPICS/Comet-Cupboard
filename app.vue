e3b9e08b0af8fc0c749c3b26d741faff264a7408
rishnarenikunta <113567515+rishnarenikunta@users.noreply.github.com>
2024-10-22 23:28:02 -0500

    fixed app.vue

--- app.vue
+++ app.vue
@@ -2,20 +2,5 @@
 .flex.flex-col.min-h-screen.font-utd
     CometHeader.sticky.top-0
     NuxtPage.mx-5.sm_mx-24.grow
-    ItemCard(
-        name="apple"
-        imgURL="https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Red_Apple.jpg/1130px-Red_Apple.jpg"
-        itemID="12345"
-        currentCount="20"
-        typeOfCard="INVENTORY"
-    )
-    ItemCard(
-        name="apple"
-        imgURL="https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Red_Apple.jpg/1130px-Red_Apple.jpg"
-        itemID="12345"
-        currentCount="20"
-        typeOfCard="SHOPPING"
-    )
-
     CometFooter
 </template>
