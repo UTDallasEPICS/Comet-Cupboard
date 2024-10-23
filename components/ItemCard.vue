07be88a5980f5ab2d60caeed62e532afffad1f18
rishnarenikunta <113567515+rishnarenikunta@users.noreply.github.com>
2024-10-22 21:15:53 -0500

    Changes based on PR

--- components/ItemCard.vue
+++ components/ItemCard.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
-  div.p-4.w-72(v-if="typeOfCard === 'inventory'")
+  div.p-4.w-72(v-if="typeOfCard === 'INVENTORY'")
     div.relative
       div.flex.justify-center
-        img.w-250.h-250.shadow-md.object-cover.rounded-md(:src="imgURL" :alt="name")
+        img.w-64.h-64.shadow-md.object-cover.rounded-md(:src="imgURL" :alt="name")
       
       div.absolute.top-2.right-2.bg-gray-300.rounded-full.p-3.shadow-md.cursor-pointer.w-13.h-13(@click="toggleDropdown")
         div.flex.items-center.justify-center.flex-row.space-x-1
@@ -16,17 +16,20 @@
           li.flex.justify-center.items-center.px-1.py-1.cursor-pointer.hover(@click="removeItem").border-b.border-gray-400 Remove
           li.flex.justify-center.items-center.px-1.py-1.cursor-pointer.hover(@click="itemDeal") Item Deal
 
-    div.flex.justify-center.items-center.mt-2
-      div.border.border-utd-orange.shadow-md.rounded-l-full.px-7.py-1.flex.items-center
+    div.flex.justify-center.items-center.mt-2.w-64
+      div.border.border-utd-orange.shadow-md.rounded-l-full.px-5.py-1.flex-grow.flex.items-center.justify-center
         span.text-sm.font-semibold.text-black {{ currentCount }}
-      div.border.border-utd-orange.bg-utd-orange.border-l-0.bg-opacity-20.shadow-md.rounded-r-full.px-7.py-1.flex.items-center
+      div.border.border-utd-orange.bg-utd-orange.border-l-0.bg-opacity-20.shadow-md.rounded-r-full.px-5.py-1.flex-grow.flex.items-center.justify-center
         span.text-sm.font-semibold.text-utd-orange +{{ change || '0' }}
 
     div.flex.justify-center.items-center.space-x-6.mt-3
       button.bg-utd-green.text-white.shadow-md.rounded-full.p-2.w-10.h-10.flex.items-center.justify-center(@click="decrementCount")
         span.text-xl.font-bold -
 
-      div.text-xl.font-semibold.text-gray-900.border-b-2.border-black.w-10.flex.justify-center {{ '1' }}
+      input.text-xl.font-semibold.text-gray-900.border-b-2.border-black.w-10.flex.text-center(
+		v-model="numOfItems"
+		type="text"
+	  )
 
       button.bg-utd-green.text-white.shadow-md.rounded-full.p-2.w-10.h-10.flex.items-center.justify-center(@click="incrementCount")
         span.text-xl.font-bold +
@@ -34,9 +37,9 @@
     div.mt-4.text-center
       p.text-xl {{ name }}
 
-  div.p-4.w-72(v-if="typeOfCard === 'shopping'")
+  div.p-4.w-72(v-if="typeOfCard === 'SHOPPING'")
     div.flex.justify-center
-      img.w-250.h-250.shadow-md.object-cover.rounded-md(:src="imgURL" :alt="name")    
+      img.w-64.h-64.shadow-md.object-cover.rounded-md(:src="imgURL" :alt="name")    
 
     div.mt-4
       button.bg-utd-green.text-white.py-1.rounded-full.w-full(@click="addToCart") Add to Cart
@@ -47,6 +50,8 @@
 
 
 <script lang="ts" setup>
+import { ref } from 'vue';
+
 const props = defineProps({
 	name: {
 		type: String,
@@ -74,6 +79,7 @@ const props = defineProps({
 })
 
 const isDropdownOpen = ref(false);
+const numOfItems = ref(1); 
 
 const toggleDropdown = () => {
 	isDropdownOpen.value = !isDropdownOpen.value; // Toggle the dropdown state
