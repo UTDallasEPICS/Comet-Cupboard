573874629d65b86b74fbea9255189667ea8b1fde
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2025-03-18 21:24:28 -0500

    adjust data page APIs and fix seed data

--- pages/data.vue
+++ pages/data.vue
@@ -1,18 +1,29 @@
 <template lang="pug">
 div
-	button(@click="temporaryArray.push(1)") Add
 	ClientOnly
-		PlotlyChart(v-for="i in temporaryArray" :data="data")
+		PlotlyChart(:data="aaaaaaa")
 </template>
 
 <script setup>
-const temporaryArray = ref([0])
+const { data: itemsIn } = await useFetch("/api/data/itemsIn")
+const { data: itemsOut } = await useFetch("/api/data/itemsOut")
 
-const data = [
-	{
-		x: ["giraffes", "orangutans", "monkeys"],
-		y: [20, 14, 23],
-		type: "bar",
-	},
-]
+const lol = Object.groupBy(itemsOut.value, (order) => {
+	const date = new Date(Date.parse(order.date))
+	return new Date(date.getFullYear(), date.getMonth())
+})
+
+const uniqueUsersPerMonth = Object.keys(lol).map((year_month) => {
+	const uniqueUsers = new Set(lol[year_month].map((order) => order.netID))
+	return {
+		x: year_month,
+		y: uniqueUsers.size
+	}
+})
+
+const aaaaaaa = [{
+	x: uniqueUsersPerMonth.map((x) => x.x),
+	y: uniqueUsersPerMonth.map((x) => x.y),
+	type: "linechart"
+}]
 </script>
---
+++ server/api/data/itemsOut.get.ts
@@ -0,0 +1,19 @@
+export default defineEventHandler(async (event) => {
+	const orderItems = await event.context.prisma.order.findMany({
+        include: {
+            OrderItems: {
+                select: {
+                    count: true,
+                    Item: {
+                        select: {
+                            itemID: true,
+                            name: true,
+                            categoryName: true,
+                        },
+                    },
+                },
+            },
+        }
+    })
+	return orderItems
+})
