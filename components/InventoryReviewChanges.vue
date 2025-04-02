3120bab2eabd3ded668e49d261fea7ce5c31a8ce
kobebean2 <samermuhanna8@gmail.com>
2025-04-02 15:46:27 -0500

    add source moved next to source drop down instead of inside

--- components/InventoryReviewChanges.vue
+++ components/InventoryReviewChanges.vue
@@ -2,12 +2,7 @@
 div.flex.flex-col.pt-2.pb-5.px-5.overflow-y-auto.overscroll-contain
 	container.flex
 		ControlsSource(@sourceChange="(selectedSource) => (source = selectedSource)").max-md_order-first.max-md_pb-3
-		input(
-			placeholder="Enter new source"
-			type="text"
-			v-model="newSource"
-			@keydown.enter="addSource"
-		).flex-1.p-2.border.rounded-lg.outline-none
+		input(placeholder="Enter new source" type="text" v-model="newSource" @keydown.enter="addSource").flex-1.p-2.border.rounded-lg.outline-none
 		button(@click="addSource").bg-utd-green.text-white.rounded-full.w-12.h-10.flex.place-content-center.place-items-center.hover_drop-shadow-standard
 			PlusIcon.fill-white.stroke-white.h-6
 	div.divide-y.divide-cupboard-lg.mb-5
