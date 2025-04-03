e24f483160b4364fc1cbc927a6f9cc2abc4edd08
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2025-04-02 21:48:39 -0500

    Hamburger (#106)
    * going to my computer
    
    * replaced navbar with headlessui menu and placed it into hamburger

--- components/Transitions/DropDown.vue
+++ components/Transitions/DropDown.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 Transition(
-	enter-active-class="transition duration-100 ease-out pointer-events-none"
+	enter-active-class="transition duration-200 ease-out pointer-events-none"
 	enter-from-class="transform -translate-y-7 opacity-0"
 	enter-to-class="transform translate-y-0 opacity-100"
 	leave-active-class="transition duration-75 ease-in pointer-events-none"
