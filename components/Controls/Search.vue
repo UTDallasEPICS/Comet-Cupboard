e7a9dbb107603f0b480f0ec8c7558d63ad865bce
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2025-04-16 14:53:22 -0500

    I think this is right

--- components/Controls/Search.vue
+++ components/Controls/Search.vue
@@ -2,10 +2,10 @@
 div.relative.flex.grow
 	Combobox(nullable v-model="searchTerm")
 		ComboboxInput(placeholder="Search" @change="searchTerm = $event.target.value.trim()" :displayValue="(searchTerm) => searchTerm").input.pl-10.w-full
-		ComboboxButton.absolute.left-0.top-3
-			MagnifyingGlassIcon.h-7.fill-black.pointer-events-none
-		button(@click="clear").absolute.right-0.top-3
-			XMarkIcon.h-7.fill-black.pointer-events-none
+		ComboboxButton.absolute.left-0.top-3.remove-button-effects
+			MagnifyingGlassIcon.h-7.fill-black
+		button(@click="clear").absolute.right-0.top-3.remove-button-effects
+			XMarkIcon.h-7.fill-black
 		// list filtered results (filtered in prisma api call)
 		TransitionsDropDown
 			ComboboxOptions.absolute.top-14.z-10.bg-white.drop-shadow-standard.rounded-xl.w-full.max-h-96.divide-y.divide-cupboard-lg.overflow-y-scroll.overscroll-contain
@@ -24,21 +24,23 @@ import { MagnifyingGlassIcon, XMarkIcon } from "@heroicons/vue/24/solid"
 const emit = defineEmits(["searchTermChange"])
 
 const searchTerm = ref("")
-const { data: holder } = await useFetch("/api/controls/filteredItems", { query: { term: searchTerm.value } }) //Holds all the items to be displayed when the search term is empty
-const filteredResults = ref([])
+const { data: holder } = await useFetch("/api/controls/filteredItems", {
+	query: { term: searchTerm.value !== null && searchTerm.value !== "" ? searchTerm.value : null },
+}) //Holds all the items to be displayed when the search term is empty
+// const filteredResults = ref([])
+
+const filteredResults = computed(() => {
+	//If the search term is empty, reset the filtered results to the holder value
+	if (searchTerm.value === null || searchTerm.value === "") {
+		return holder.value || []
+	}
+	return holder.value.filter((item) => item.name.toLowerCase().includes(searchTerm.value.toLowerCase()))
+})
 
 // emit searchTerm changes
-watch(searchTerm, async (newValue) => {
-	//If the search term is empty, reset the filtered results to the holder value
-	if (searchTerm.value === null || newValue === "") {
-		searchTerm.value = ""
-		filteredResults.value = holder.value || []
-	}
-
+watch(searchTerm, (newValue) => {
 	// return id of the searched for entry
 	emit("searchTermChange", searchTerm.value)
-	const { data } = await useFetch("/api/controls/filteredItems", { query: { term: newValue } })
-	filteredResults.value = data.value || []
 })
 
 const clear = () => {
