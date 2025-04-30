e2f0bb3e6b064e3b14c22f433482206c10e26f89
kobebean2 <samermuhanna8@gmail.com>
2025-04-30 16:13:58 -0500

    field.post.ts

--- server/api/inventory/field.post.ts
+++ server/api/inventory/field.post.ts
@@ -1,35 +1,34 @@
 import { z } from "zod"
 
 const schema = z.object({
-    source: z.string(),
-    fields: z.array(z.object({ name: z.string() })),
+	source: z.string(),
+	fields: z.array(z.object({ name: z.string() })),
 })
 
 const validateSchema = schema.strict().required()
 
 export default defineEventHandler(async (event) => {
-    const result = await readValidatedBody(event, (body) => validateSchema.safeParse(body))
-    if (!result.success) {
-        throw createError({ statusCode: 400, statusMessage: "Invalid request body" })
-    }
-    const { source, fields } = result.data
+	const result = await readValidatedBody(event, (body) => validateSchema.safeParse(body))
+	if (!result.success) {
+		throw createError({ statusCode: 400, statusMessage: "Invalid request body" })
+	}
+	const { source, fields } = result.data
 
-    await event.context.prisma.source.findUnique({
-        where: {
-            name: source,
-        },
-    })
+	await event.context.prisma.source.findUnique({
+		where: {
+			name: source,
+		},
+	})
 
-    await event.context.prisma.field.createMany({
-        data: fields.map((field) => ({
-            name: field.name,
-            sourceName: source,
-        })),
-    })
+	await event.context.prisma.field.createMany({
+		data: fields.map((field) => ({
+			name: field.name,
+			sourceName: source,
+		})),
+	})
 
-    await event.context.prisma.source.findUnique({
-        where: { name: source },
-        include: { Fields: true },
-    })
-
+	await event.context.prisma.source.findUnique({
+		where: { name: source },
+		include: { Fields: true },
+	})
 })
