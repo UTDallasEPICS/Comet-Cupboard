215b874ea6501df04d0e6800453850214ae0f50c
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2025-04-30 15:43:57 -0500

    format

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-div.flex.flex-col.min-h-screen.font-utd.selection_bg-utd-orange.selection_text-white(style="background-color: #f4f4f4")
+div(style="background-color: #f4f4f4").flex.flex-col.min-h-screen.font-utd.selection_bg-utd-orange.selection_text-white
 	NuxtLoadingIndicator(color="#E4701E")
 	CometHeader.sticky.top-0
 	NuxtPage.mx-5.sm_mx-24.mt-4.mb-4.grow
