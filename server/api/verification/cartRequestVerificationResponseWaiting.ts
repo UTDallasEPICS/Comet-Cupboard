5d365c86f9aa4ead6273e88d7ec9467c919cce55
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2025-05-14 18:32:58 -0500

    allow multiple cart eventsources to be created for the same netid (#139)
    * allow multiple cart eventsources to be created for the same netid
    
    * woops

--- server/api/verification/cartRequestVerificationResponseWaiting.ts
+++ server/api/verification/cartRequestVerificationResponseWaiting.ts
@@ -1,7 +1,9 @@
 import { userMap } from "~/server/utils/cartVerificationUtil"
+import { nanoid } from "nanoid"
 
 export default defineEventHandler(async (event) => {
 	const eventStream = createEventStream(event)
+	const eventStreamID = nanoid()
 
 	const interval = setInterval(async () => {
 		await eventStream.push(`ping-pong`)
@@ -10,13 +12,17 @@ export default defineEventHandler(async (event) => {
 	eventStream.onClosed(async () => {
 		clearInterval(interval)
 		await eventStream.close()
-		if (userMap[event.context.user.netID]) {
+		if (userMap[event.context.user.netID][eventStreamID]) {
 			// eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-			delete userMap[event.context.user.netID]
+			delete userMap[event.context.user.netID][eventStreamID]
 		}
 	})
 
-	userMap[event.context.user.netID] = eventStream
+	if (!userMap[event.context.user.netID]) {
+		userMap[event.context.user.netID] = {}
+	}
+
+	userMap[event.context.user.netID][eventStreamID] = eventStream
 
 	return eventStream.send()
 })
--- server/api/verification/pendingCartsUpdate.ts
+++ server/api/verification/pendingCartsUpdate.ts
@@ -1,7 +1,9 @@
 import { volunteerMap } from "~/server/utils/cartVerificationUtil"
+import { nanoid } from "nanoid"
 
 export default defineEventHandler(async (event) => {
 	const eventStream = createEventStream(event)
+	const eventStreamID = nanoid()
 
 	const interval = setInterval(async () => {
 		await eventStream.push(`ping-pong`)
@@ -10,13 +12,17 @@ export default defineEventHandler(async (event) => {
 	eventStream.onClosed(async () => {
 		clearInterval(interval)
 		await eventStream.close()
-		if (volunteerMap[event.context.user.netID]) {
+		if (volunteerMap[event.context.user.netID][eventStreamID]) {
 			// eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-			delete volunteerMap[event.context.user.netID]
+			delete volunteerMap[event.context.user.netID][eventStreamID]
 		}
 	})
 
-	volunteerMap[event.context.user.netID] = eventStream
+	if (!volunteerMap[event.context.user.netID]) {
+		volunteerMap[event.context.user.netID] = {}
+	}
+
+	volunteerMap[event.context.user.netID][eventStreamID] = eventStream
 
 	return eventStream.send()
 })
