52ec2b72dcdf151e608f6238da26a11aa5814253
kobebean2 <samermuhanna8@gmail.com>
2025-05-14 16:05:29 -0500

    merge conflict

--- components/InventoryReviewChanges.vue
+++ components/InventoryReviewChanges.vue
@@ -26,11 +26,11 @@ div.flex.flex-col.pt-2.pb-5.px-5.overflow-y-auto.overscroll-contain.relative
 			div.flex.flex-row
 				div.w-screen
 					// page control components
-					div.flex.flex-row.md_flex-row.pb-3.md_space-x-3 
+					div.flex.flex-row.md_flex-row.pb-3.md_space-x-3
 						div.skeleton.w-full.h-12.md_w-44.rounded-xl
 					//Boxes
-					div.flex.flex-col.divide-y.divide-cupboard-lg.mb-5 
-						div.skeleton.w-full.h-12.md_w-44.rounded-xl
+					div.flex.flex-col.divide-y.divide-cupboard-lg.mb-5
+						div.skeleton.w-full.h-20.rounded-xl
 					//Buttons
 					div.flex.flex-row.justify-end.space-x-5
 						div.skeleton.w-40.h-12.rounded-xl
