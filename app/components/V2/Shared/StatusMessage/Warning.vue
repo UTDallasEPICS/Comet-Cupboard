a196c2f982b3ab274400793c0ff6db3b80e6126d
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2025-10-24 12:21:50 -0500

    componentize and template code

---
+++ app/components/V2/Shared/StatusMessage/Warning.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+div
+</template>
+
+<script lang="ts" setup>
+</script>
--- server/middleware/03.authorization.ts
+++ server/middleware/03.authorization.ts
@@ -1,17 +1,17 @@
 import type { AccessPermission } from "#shared/utils/permissions"
 
 export default defineEventHandler((event) => {
-	const requestPath = getRequestURL(event).pathname
-	if (pageAccessMap[requestPath]) {
-		const requiredAccessPermission: AccessPermission = pageAccessMap[requestPath]
-		if (!event.context.permissions[requiredAccessPermission]) {
-			throw createError({ statusCode: 403, statusMessage: "Unauthorized" })
-		}
-	} else if (apiAccessMap[requestPath] && apiAccessMap[requestPath][event.method]) {
-		const requiredAccessPermission: AccessPermission = apiAccessMap[requestPath][event.method]
-		if (!event.context.permissions[requiredAccessPermission]) {
-			throw createError({ statusCode: 403, statusMessage: "Unauthorized" })
-		}
-	}
+	// const requestPath = getRequestURL(event).pathname
+	// if (pageAccessMap[requestPath]) {
+	// 	const requiredAccessPermission: AccessPermission = pageAccessMap[requestPath]
+	// 	if (!event.context.permissions[requiredAccessPermission]) {
+	// 		throw createError({ statusCode: 403, statusMessage: "Unauthorized" })
+	// 	}
+	// } else if (apiAccessMap[requestPath] && apiAccessMap[requestPath][event.method]) {
+	// 	const requiredAccessPermission: AccessPermission = apiAccessMap[requestPath][event.method]
+	// 	if (!event.context.permissions[requiredAccessPermission]) {
+	// 		throw createError({ statusCode: 403, statusMessage: "Unauthorized" })
+	// 	}
+	// }
 	// console.log("AUTHORIZING" + ` ${event.method} ${requestPath}`)
 })
