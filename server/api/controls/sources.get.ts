3cfa8b05cbf6d2228024260f290aa1ceeb180725
Froakiein2022 <tony.tran3622@gmail.com>
2025-12-07 20:10:38 -0600

    Added the archived attribute to the Source model and changed sources.get.ts to allow the conditional viewing of archived sources.

--- server/api/controls/sources.get.ts
+++ server/api/controls/sources.get.ts
@@ -1,6 +1,28 @@
+import { z } from "zod"
+
+const schema = z.object({
+	includeArchived: z.string().default("false"),
+})
+
+const validateSchema = schema.strict().partial()
+
 export default defineEventHandler(async (event) => {
+	const queries = await getValidatedQuery(event, (query) => validateSchema.safeParse(query))
+	if (!queries.success) {
+		throw createError({ statusCode: 400, statusMessage: "Invalid request parameters" })
+	}
+	const { includeArchived } = queries.data
+
+	//Only administrators can view archived sources
+	if (includeArchived === "true" && !event.context.permissions[AccessPermission.ADMIN]) {
+		throw createError({ statusCode: 403, statusMessage: `User ${event.context.user.netID} is unauthorized to view archived sources` })
+	}
+
 	// retrieve the list of sources from the db
 	const sources = await event.context.prisma.source.findMany({
+		where: {
+			...((includeArchived === "false") ? { archived: false } : {}),
+		},
 		include: {
 			Fields: true,
 		},
