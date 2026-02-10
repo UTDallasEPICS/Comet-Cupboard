53944837a5ce5c22ca62bb05f73038f0cd173c13
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2026-02-10 02:41:09 -0600

    fix footer

--- app/components/Shared/CometFooter.vue
+++ app/components/Shared/CometFooter.vue
@@ -1,99 +1,42 @@
 <template>
-	<UFooter class="bg-final-cancel-gray">
-		<template #default>
-			<div class="mx-7 my-5 flex flex-col place-content-evenly text-white max-sm:space-y-7 sm:flex-row sm:space-x-7">
+	<UFooter class="bg-final-cancel-gray text-white">
+		<div class="flex flex-col gap-8 py-6 sm:flex-row sm:items-start sm:justify-between">
+			<img src="/CometCupboardLogo2.png" alt="Comet Cupboard" class="h-14" />
+
+			<div class="space-y-2">
+				<p class="font-bold">Contact</p>
+				<p>{{ room }}</p>
+				<p>{{ email }}</p>
+				<p>{{ phone }}</p>
+			</div>
+
+			<div class="space-y-2">
+				<p class="font-bold">Hours</p>
+
 				<div>
-					<img src="/CometCupboardLogo2.png" class="h-14" >
+					<p class="font-bold">Monday, Wednesday, Friday</p>
+					<p>10 AM - 4 PM</p>
 				</div>
-				<div class="space-y-1">
-					<h2 class="">Contact</h2>
-					<div>
-						<button class="remove-button-effects space-x-1 text-left" @click="handleRoom">
-							<p class="inline-flex">Location:</p>
-							<p class="inline-flex">{{ room }}</p>
-							<div v-show="roomCopied" class="inline-flex">
-								<ClipboardDocumentCheckIcon class="size4 w-4 fill-white" />
-							</div>
-						</button>
-					</div>
-					<div>
-						<button class="remove-button-effects space-x-1 text-left" @click="handleEmail">
-							<p class="inline-flex">Email:</p>
-							<p class="inline-flex">{{ email }}</p>
-							<div v-show="emailCopied" class="inline-flex">
-								<ClipboardDocumentCheckIcon class="size4 w-4 fill-white" />
-							</div>
-						</button>
-					</div>
-					<div>
-						<button class="remove-button-effects space-x-1 text-left" @click="handlePhone">
-							<p class="inline-flex">Phone</p>
-							<p class="inline-flex">{{ phone }}</p>
-							<div v-show="phoneCopied" class="inline-flex">
-								<ClipboardDocumentCheckIcon class="size4 w-4 fill-white" />
-							</div>
-						</button>
-					</div>
-				</div>
-				<div class="space-y-1">
-					<h2 class="">Hours</h2>
-					<div class="space-y-3">
-						<div>
-							<p>Monday, Wednesday, Friday:</p>
-							<p>10 AM - 4 PM</p>
-						</div>
-						<div>
-							<p>Tuesday, Thursday:</p>
-							<p>11 AM - 5 PM</p>
-						</div>
-					</div>
-				</div>
-				<div class="space-y-1">
-					<h2 class="">Connect With Us</h2>
-					<NuxtLink :to="instagram">
-						<img src="/instagramIcon.svg" class="h-10" >
-					</NuxtLink>
+
+				<div>
+					<p class="font-bold">Tuesday, Thursday</p>
+					<p>11 AM - 5 PM</p>
 				</div>
 			</div>
-		</template>
+
+			<div class="space-y-2">
+				<p class="font-bold">Connect With Us</p>
+				<ULink :to="instagram">
+					<UIcon name="mdi:instagram" class="size-6 text-white" />
+				</ULink>
+			</div>
+		</div>
 	</UFooter>
 </template>
 
 <script setup lang="ts">
-import { ClipboardDocumentCheckIcon } from "@heroicons/vue/24/solid"
-
-const roomCopied = ref(false)
-const emailCopied = ref(false)
-const phoneCopied = ref(false)
-
-const room = "MC 1.604"
-const email = "cupboard@utdallas.edu"
-const phone = "972-883-6613"
+const room = "Location: MC 1.604"
+const email = "Email: cupboard@utdallas.edu"
+const phone = "Phone: 972-883-6613"
 const instagram = "https://www.instagram.com/cometcupboard/"
-
-// contact info copy user-feedback
-const handleRoom = () => {
-	navigator.clipboard.writeText(room)
-	roomCopied.value = true
-	setTimeout(roomAlert, 2500)
-}
-const roomAlert = () => {
-	roomCopied.value = false
-}
-const handleEmail = () => {
-	navigator.clipboard.writeText(email)
-	emailCopied.value = true
-	setTimeout(emailAlert, 2500)
-}
-const emailAlert = () => {
-	emailCopied.value = false
-}
-const handlePhone = () => {
-	navigator.clipboard.writeText(phone)
-	phoneCopied.value = true
-	setTimeout(phoneAlert, 2500)
-}
-const phoneAlert = () => {
-	phoneCopied.value = false
-}
 </script>
