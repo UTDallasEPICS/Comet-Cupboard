2c2faa30b380e662ae12613b1c83c1f1da4c3afd
AaronDcunha <aaronpdcunha@hotmail.com>
2026-03-11 16:39:44 -0500

    Adding the files and a helper function

---
+++ server/api/volunteer/emergency-bag/emergencyBag.get.ts
@@ -0,0 +1,17 @@
+//import { z } from "zod"
+import { prisma } from "#server/utils/db"
+import { defineSafeHandler } from "#server/utils/handler"
+
+export default defineSafeHandler(async (event) => {
+    // TODO: Implement emergency bag retrieval logic
+    const emBags = [];
+    /*
+    await prisma.TABLENAMEFORBAG.findMany({
+        select: {
+            //Whatever
+        }
+    });
+    */
+
+    return emBags;
+})
