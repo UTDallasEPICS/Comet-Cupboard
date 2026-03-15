d2f5ea6f7e0a026611f01a1b9f9defdb76a9cd06
AaronDcunha <aaronpdcunha@hotmail.com>
2026-03-15 17:09:09 -0500

    Added get endpoint for emergency bags

--- server/api/volunteer/emergency-bag/emergencyBags.get.ts
+++ server/api/volunteer/emergency-bag/emergencyBags.get.ts
@@ -3,26 +3,7 @@ import { prisma } from "#server/utils/db"
 import { defineSafeHandler } from "#server/utils/handler"
 
 export default defineSafeHandler(async (event) => {
-    // TODO: Implement emergency bag retrieval logic
-    const emBags = await prisma.emergencyBag.findMany({
-        select: {
-            bagID: true,
-            Location: true,
-            locationName: true,
-            bagCategory: true,
-            EmergencyBagItems: true,
-            bagDescription: true,
-            expiryDate: true,
-            label: true
-        }
-    });
-    /*
-    await prisma.BAG.findMany({
-        select: {
-            //Whatever
-        }
-    });
-    */
+    const emBags = await prisma.emergencyBag.findMany();
 
     return emBags;
 })
