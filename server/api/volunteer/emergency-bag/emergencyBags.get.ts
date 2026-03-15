48b2a73696c8aed7b828292570b962f6de406311
AaronDcunha <aaronpdcunha@hotmail.com>
2026-03-15 17:07:32 -0500

    Renaming emergencyBag.get file to emergencyBags.get

--- server/api/volunteer/emergency-bag/emergencyBags.get.ts
+++ server/api/volunteer/emergency-bag/emergencyBags.get.ts
@@ -4,7 +4,18 @@ import { defineSafeHandler } from "#server/utils/handler"
 
 export default defineSafeHandler(async (event) => {
     // TODO: Implement emergency bag retrieval logic
-    const emBags = [];
+    const emBags = await prisma.emergencyBag.findMany({
+        select: {
+            bagID: true,
+            Location: true,
+            locationName: true,
+            bagCategory: true,
+            EmergencyBagItems: true,
+            bagDescription: true,
+            expiryDate: true,
+            label: true
+        }
+    });
     /*
     await prisma.BAG.findMany({
         select: {
