e1e02618ff26e05edc79a456fb5469fdd19cd956
AaronDcunha <aaronpdcunha@hotmail.com>
2026-03-18 21:38:00 -0500

    Changed delete endpoint to only delete one bag

--- server/api/volunteer/emergency-bag/emergencyBags.delete.ts
+++ server/api/volunteer/emergency-bag/emergencyBags.delete.ts
@@ -2,74 +2,76 @@ import { z } from "zod"
 import { prisma } from "#server/utils/db"
 import { defineSafeHandler } from "#server/utils/handler"
 import { validateBody } from "#server/utils/validation"
+import { StatusCodes } from "http-status-codes"
 
 const schema = z
   .object({
-    bagIDs: z.array(z.string()),
+    bagID: z.string(),
   })
   .strict()
   .required()
 
 export default defineSafeHandler(async (event) => {
     try{
-        const {bagIDs} = await validateBody(event, schema);
-        const bags = await prisma.emergencyBag.findMany({
-            where: {
-                bagID: {in: bagIDs}
-            },
+        const {bagID} = await validateBody(event, schema);
+        console.log(bagID);
+        const bag = await prisma.emergencyBag.findUnique({
+            where: {bagID},
             include: {
                 EmergencyBagItems: true
             }
         });
 
-        if (bags.length == 0){
-            return bags;
+        if (!bag){
+            //ERROR
+            throw createError({
+                statusCode: StatusCodes.NOT_FOUND,
+                statusMessage: "Emergency bag not found"
+            });
         }
 
         //Transfer to Issued Bag
-        await prisma.issuedEmergencyBag.createMany({
-            data: bags.map((bag)=>({
+        await prisma.issuedEmergencyBag.create({
+            data: {
                 bagID: bag.bagID,
                 location: bag.locationName ?? "Activity Center", //Default??? It isnt accepting null..
                 bagCategory: bag.bagCategory,
                 bagDescription: bag.bagDescription,
                 expiryDate: bag.expiryDate,
                 label: bag.label
-            }))
+            }
         });
 
         //Transfer Items to Issued Items
-        const allItems = bags.flatMap((bag) =>
-            bag.EmergencyBagItems.map((item) => ({
-                itemID: item.itemID,
-                bagID: bag.bagID,
-                count: item.count
-            }))
-        );
-        await prisma.issuedEmergencyBagItem.createMany({
-            data: allItems
-        });
+        const items = bag.EmergencyBagItems.map((item) => ({
+            itemID: item.itemID,
+            bagID: bag.bagID,
+            count: item.count
+        }));
 
-        //Delete Items
-        await prisma.emergencyBagItem.deleteMany({
-            where: {
-                bagID: { in: bagIDs }
-            }
-        });
+        if (items.length > 0){
+            await prisma.issuedEmergencyBagItem.createMany({
+                data: items
+            });
+
+            //Delete Items
+            await prisma.emergencyBagItem.deleteMany({
+                where: {
+                    bagID
+                }
+            });
+        }
+        
 
         //Delete Bags
-        await prisma.emergencyBag.deleteMany({
-            where: {
-                bagID: { in: bagIDs }
-            }
+        await prisma.emergencyBag.delete({
+            where: { bagID }
         });
+
+        return { success: true };
 
     }catch (err){
         console.log(err);
-        return [];
+        throw err;
     }
-    
-
-
-    return [];
 })
