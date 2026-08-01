c396c4d3b88ffde1af8742859c37f9218645fe96
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2026-08-01 04:51:35 -0400

    env for internal container networking

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
 	devtools: { enabled: true },
 	modules: ["@nuxt/eslint", "@pinia/nuxt", "@nuxt/ui"],
 	css: ["~/assets/css/main.css"],
-	runtimeConfig: { EPICS_SSO_BASE_URL: "", public: { LOCAL_URL: "", NODE_ENV: "" } },
+	runtimeConfig: { EPICS_SSO_BASE_URL: "", EPICS_SSO_INTERNAL_URL: "", public: { LOCAL_URL: "", NODE_ENV: "" } },
 	features: { inlineStyles: false },
 	ui: {
 		colorMode: false,
